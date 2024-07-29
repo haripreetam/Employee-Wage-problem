d66dedcefe67361c38939fadb4586bf32b4c8fb0
Haripreetam <haripreetam333@gmail.com>
2024-07-29 15:20:28 +0530

    [Haripreetam Reddy] ADD:python script for employee wage attendence

--- employee_wage.py
+++ employee_wage.py
@@ -1,1 +1,13 @@
-print("Welcome to Employee Wage Program")
+
+import random
+
+def check_attendence():
+    attendence = random.randint(0,1)
+    if attendence == 1:
+        print("Employee is present")
+    else:
+        print("Employee is absent")
+
+if __name__  == "__main__":
+    print("Welcome to Employee Wage Program")
+    check_attendence()
